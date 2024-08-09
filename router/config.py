b0b332219b7eb7e5318b17b33a5e14342c933e7c
Muu <muu@vip.qq.com>
2024-08-09 11:46:05 +0800

    refactor: post /config request error returns error details

--- router/config.py
+++ router/config.py
@@ -14,8 +14,8 @@ router = APIRouter()
 async def set_config(request: ConfigModel):
     try:
         ConfigManager().load_config(request)
-    except Exception:
-        return APIResponse(data={"result": False, "message": define.ConfigMsg.ILLEGAL_SOURCE})
+    except Exception as e:
+        return APIResponse(data={"result": False, "message": define.ConfigMsg.ILLEGAL_SOURCE, "error": str(e})
     return APIResponse(data={"result": True, "message": define.IMsg.OK})
 
 
